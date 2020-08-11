6505554d3cee0a03415b2b6bbf7719b53acc9af0
NoumanSabir86 <35195045+NoumanSabir86@users.noreply.github.com>
2020-08-11 16:28:49 +0500

    Add files via upload

---
+++ build/precache-manifest.8539d13f7fc96e467b63370ba6b10906.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "577f16d3aaafdbed803df52488a1dd00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cb1ea16f0a01aa5879b",
+    "url": "/static/css/2.efb89804.chunk.css"
+  },
+  {
+    "revision": "8fe6a8ec0e3c8969c9c4",
+    "url": "/static/css/main.4177ca81.chunk.css"
+  },
+  {
+    "revision": "6cb1ea16f0a01aa5879b",
+    "url": "/static/js/2.3f4868fd.chunk.js"
+  },
+  {
+    "revision": "979282f1b892ea63eea7e1c5e3b4fb7e",
+    "url": "/static/js/2.3f4868fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fe6a8ec0e3c8969c9c4",
+    "url": "/static/js/main.83ecf18f.chunk.js"
+  },
+  {
+    "revision": "6390dfa7ff95dff7d07a",
+    "url": "/static/js/runtime-main.f1322243.js"
+  },
+  {
+    "revision": "9ffac8a79824c109595f73fc5ec057d3",
+    "url": "/static/media/headset.9ffac8a7.jpg"
+  },
+  {
+    "revision": "4203dfd947c11994001497f044bcdf82",
+    "url": "/static/media/laptop1.4203dfd9.png"
+  },
+  {
+    "revision": "3a8e79cfd48e8518ec2f486487d008d8",
+    "url": "/static/media/watch2.3a8e79cf.jpg"
+  }
+]);
---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var router = express.Router();
+
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+module.exports = router;
