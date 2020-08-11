e1f271e3c5b4ebe1d5f25690700db7a16c9db55f
NoumanSabir86 <35195045+NoumanSabir86@users.noreply.github.com>
2020-08-11 16:37:45 +0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@ var router = express.Router();
 
 const path = require("path");
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "../build")));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname + "../build/index.html"));
 });
 
 /* GET home page. */
